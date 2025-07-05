5d21625ce95698a8ad3f263d7f8aa0fbe360c451
Lev1reG <derentanaphan@mail.ugm.ac.id>
2025-07-05 08:28:43 +0700

    feat: setup layout components

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,11 +1,16 @@
 import Home from "@/pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout  />}>
+          <Route index element={<Home />} />
+          {/* Add more routes here as needed */}
+          {/* Example: <Route path="about" element={<About />} /> */}
+        </Route>
       </Routes>
     </BrowserRouter>
   );
