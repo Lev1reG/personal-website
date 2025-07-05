6eba84213f9d3b7bbfdf97f5749ff7a62c536c7b
Lev1reG <derentanaphan@mail.ugm.ac.id>
2025-07-05 12:21:40 +0700

    feat: add bg color on layout components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,17 @@
-import { Outlet } from "react-router-dom"
-import Header from "@/components/Header"
-import Footer from "@/components/Footer"
+import { Outlet } from "react-router-dom";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const Layout = () => {
   return (
-    <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="container mx-auto px-4">
-            <Outlet />
-        </main>
-        <Footer />
+    <div className="min-h-screen flex flex-col dark:bg-neutral-900 bg-neutral-100">
+      <Header />
+      <main className="container mx-auto px-4">
+        <Outlet />
+      </main>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
