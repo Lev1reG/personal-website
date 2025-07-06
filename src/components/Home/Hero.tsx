2f954de2851d26f855205c8222e5b1f75990ecfe
Lev1reG <derentanaphan@mail.ugm.ac.id>
2025-07-06 12:27:34 +0700

    feat: responsive desktop hero

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -14,21 +14,21 @@ const Hero = () => {
   ];
 
   return (
-    <article className="flex flex-col items-center justify-center space-y-5 pt-28 pb-20">
-      <div className="flex flex-col items-center space-y-5">
+    <article className="flex flex-col xl:flex-row items-center justify-center xl:justify-between space-y-5 pt-28 pb-20">
+      <div className="xl:max-w-xl flex flex-col items-center xl:items-start space-y-5">
         <img
           src={ProfilePicture}
           alt="Profile Picture of Deren Tanaphan"
           className="w-36 h-36 rounded-full border border-brand-300"
         />
-        <h1 className="text-3xl font-bold text-center dark:text-neutral-100 text-neutral-900">
+        <h1 className="text-3xl lg:text-4xl xl:text-5xl font-bold text-center xl:text-left dark:text-neutral-100 text-neutral-900">
           <span className="text-brand-500">Hi, I'm Deren</span> - I Build Things
           That <span className="text-brand-500">Work </span> and{" "}
           <span className="text-brand-500">Last.</span>
         </h1>
       </div>
-      <div className="flex flex-col items-center space-y-5">
-        <h2 className="text-2xl font-bold text-center dark:text-neutral-100 text-neutral-900 min-h-[2.5rem]">
+      <div className="xl:max-w-xl flex flex-col items-center xl:items-start space-y-5">
+        <h2 className="text-2xl lg:text-3xl xl:text-4xl font-bold text-center dark:text-neutral-100 text-neutral-900 min-h-[2.5rem]">
           <TypewriterCycle
             phrases={phrases}
             typingSpeed={100}
@@ -36,19 +36,19 @@ const Hero = () => {
             pauseDuration={3000}
           />
         </h2>
-        <p className="font-medium text-base text-center leading-snug dark:text-neutral-400 text-neutral-600">
+        <p className="font-medium text-base lg:text-lg text-center xl:text-left leading-snug dark:text-neutral-400 text-neutral-600">
           Passionate about blockchain, cyber security, machine learning, and
           full stack software development. I build scalable, secure, and
           data-driven systems with a focus on clean code, privacy, and
           performance. I'm driven to create resilient technologies that
           prioritize privacy, intelligence, and real-world impact.
         </p>
-        <div className="flex flex-col items-center space-y-5">
+        <div className="flex flex-col xl:flex-row items-center space-y-5 xl:space-y-0 xl:space-x-5">
           <Button
             asChild
             variant="defaultCustom"
             size="lg"
-            className="font-bold text-lg"
+            className="font-bold text-lg xl:text-xl"
           >
             <a
               href="https://saweria.co/levireg"
