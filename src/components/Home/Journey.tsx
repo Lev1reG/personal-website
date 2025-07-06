48ff2addc52f4f6ccf6833c95bcd728a6681d542
Lev1reG <derentanaphan@mail.ugm.ac.id>
2025-07-06 13:20:42 +0700

    fix: spacing on projects & journey

--- src/components/Home/Journey.tsx
+++ src/components/Home/Journey.tsx
@@ -26,7 +26,7 @@ const Journey = () => {
 
   return (
     <article id="journey" className="flex flex-col items-center justify-center space-y-5 lg:space-y-8 py-8 lg:py-12">
-      <div className="flex flex-col items-center justify-center space-y-2">
+      <div className="flex flex-col items-center justify-center space-y-2 lg:space-y-3">
         <h1 className="text-3xl lg:text-4xl xl:text-5xl font-bold text-center dark:text-neutral-100 text-neutral-900">
           Journey
         </h1>
