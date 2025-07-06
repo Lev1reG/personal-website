833bf9d621bf49b79cee492973698a207615606a
Lev1reG <derentanaphan@mail.ugm.ac.id>
2025-07-06 12:53:34 +0700

    feat: responsive desktop education

--- src/components/Home/Education.tsx
+++ src/components/Home/Education.tsx
@@ -8,11 +8,11 @@ const Education = () => {
   ];
 
   return (
-    <article className="flex flex-col items-center justify-center space-y-5 py-8">
-      <h1 className="text-3xl font-bold text-center dark:text-neutral-100">
+    <article className="flex flex-col items-center justify-center space-y-5 lg:space-y-8 py-8 lg:py-12">
+      <h1 className="text-3xl lg:text-4xl xl:text-5xl font-bold text-center dark:text-neutral-100">
         Education
       </h1>
-      <div className="space-y-4">
+      <div className="w-full space-y-4">
         {experiences.map((exp, index) => (
           <Experience
             key={index}
@@ -34,14 +34,16 @@ interface ExperienceProps {
 
 const Experience = ({ university, degree, date }: ExperienceProps) => {
   return (
-    <div className="flex flex-col justify-center items-center space-y-2">
-      <h2 className="text-lg font-bold text-center text-neutral-900 dark:text-neutral-100">
-        {university}
-      </h2>
-      <p className="text-xs font-medium dark:text-neutral-400 text-neutral-600 text-center">
-        {date}
-      </p>
-      <p className="text-sm font-medium dark:text-neutral-300 text-neutral-700 text-center">
+    <div className="flex flex-col justify-center items-center lg:items-start space-y-2">
+      <div className="w-full flex flex-col lg:flex-row lg:justify-between items-center space-y-2 lg:space-y-0">
+        <h2 className="text-lg lg:text-2xl font-bold text-center text-neutral-900 dark:text-neutral-100">
+          {university}
+        </h2>
+        <p className="text-xs lg:text-sm font-medium dark:text-neutral-400 text-neutral-600 text-center">
+          {date}
+        </p>
+      </div>
+      <p className="text-sm lg:text-base font-medium dark:text-neutral-300 text-neutral-700 text-center">
         {degree}
       </p>
     </div>
